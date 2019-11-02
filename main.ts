39da18605b50987aad1031bcc073fcfa1b50048d
Kumar Sidharth <k.sidharth1994@gmail.com>
2019-11-03 03:19:27 +0530

    reformatting and social tags and keywords

--- main.ts
+++ main.ts
@@ -1,25 +1,36 @@
+enum DateFormat {
+    Long = 'long',
+    Numeric = 'numeric'
+};
+const HindiLocale = 'hi-IN';
 var dateContainer: HTMLSpanElement;
 var nameContainer: HTMLSpanElement;
 
 function setDate() {
     const options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
+        weekday: DateFormat.Long,
+        year: DateFormat.Numeric,
+        month: DateFormat.Long,
+        day: DateFormat.Numeric
     };
-    const date = new Date().toLocaleDateString("hi-IN", options);
+    const date = new Date().toLocaleDateString(HindiLocale, options);
     dateContainer.innerText = date;
 }
 
+/**
+ * from the URL query params
+ * takes the value of `to`
+ * changes to human readable form
+ * and replaces `_` to blank character 
+ */
 function getName (): string {
     let name = window.location.search.replace('?to=','');
-    return decodeURIComponent(name);
+    return decodeURIComponent(name).replace('_', ' ');
 }
 
 function setName() {
     let name = getName();
-    nameContainer.innerText = name.replace('_', ' ');
+    nameContainer.innerText = name;
 }
 
 function load() {
